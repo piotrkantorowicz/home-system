b2ea51d72cac8558a9d12aec868109b395468599
Piotr Kantorowicz <pkantorowicz@proton.me>
2026-03-30 22:08:15 +0200

    refactor(host): move and rename host project to apis/homesystem.rest
    Refs #24

--- src/apis/diet-planner/DietPlanner.Api/Features/DietPlans/Import/IImportExecutor.cs
+++
@@ -1,6 +0,0 @@
-namespace DietPlanner.Api.Features.DietPlans.Import;
-
-public interface IImportExecutor
-{
-    Task<ImportResultDto> ExecuteAsync(ImportDto import, string userId);
-}
--- src/apis/diet-planner/DietPlanner.Api/Features/DietPlans/Import/IImportValidator.cs
+++
@@ -1,6 +0,0 @@
-namespace DietPlanner.Api.Features.DietPlans.Import;
-
-public interface IImportValidator
-{
-    Task<ValidationResultDto> ValidateAsync(ImportDto import, string userId);
-}
